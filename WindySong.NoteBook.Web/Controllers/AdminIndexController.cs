892249ede04f6905f9019647e26b2c299fa67498
wbflike <bf.w@qq.com>
2018-01-21 23:00:55 +0800

    没啥
    Signed-off-by: wbflike <bf.w@qq.com>

--- WindySong.NoteBook.Web/Controllers/AdminIndexController.cs
+++ WindySong.NoteBook.Web/Controllers/AdminIndexController.cs
@@ -4,19 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WindySong.NoteBook.Web.Common;
+using Microsoft.AspNetCore.Authorization;
 
 namespace WindySong.NoteBook.Web.Controllers
 {
     public class AdminIndexController : AdminController
     {
+        //权限验证
+        [Authorize]
         public IActionResult Index()
         {
             return View();
         }
+        //权限验证
+        [Authorize]
         public IActionResult Main()
         {
             return View();
         }
+        //权限验证
+        [Authorize]
         public IActionResult Test()
         {
             return View();
---
+++ WindySong.NoteBook.Web/Controllers/AdminUserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WindySong.NoteBook.Web.Common;
+
+namespace WindySong.NoteBook.Web.Controllers
+{
+    public class AdminUserController : AdminController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
