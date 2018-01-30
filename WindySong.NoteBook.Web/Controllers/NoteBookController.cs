6c2f294a28d6f2529e43d3ba51c7702c1d513476
wbflike <bf.w@qq.com>
2018-01-30 22:55:52 +0800

    ctab
    Signed-off-by: wbflike <bf.w@qq.com>

---
+++ WindySong.NoteBook.Web/Controllers/NoteBookController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WindySong.NoteBook.Web.Common;
+using Microsoft.AspNetCore.Authorization;
+
+namespace WindySong.NoteBook.Web.Controllers
+{
+    //权限验证
+    [Authorize]
+    public class NoteBookController : AdminController
+    {
+        public IActionResult Tab()
+        {
+            return View();
+        }
+    }
+}
