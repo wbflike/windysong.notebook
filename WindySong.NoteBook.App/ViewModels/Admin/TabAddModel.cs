5fe16fb045c4304454d60f80ddc727d8cf7b0e76
bf.w <bf.w@qq.com>
2018-02-08 18:49:50 +0800

    Signed-off-by: bf.w <bf.w@qq.com>

--- WindySong.NoteBook.App/ViewModels/Admin/TabAddModel.cs
+++ WindySong.NoteBook.App/ViewModels/Admin/TabAddModel.cs
@@ -14,18 +14,18 @@ namespace WindySong.NoteBook.App.ViewModels.Admin
         /// 每页显示多少条数据
         /// </summary>
         [Required(ErrorMessage = "名称必填!")]
-        public string name { get; set; }
+        public string addName { get; set; }
 
         /// <summary>
         /// 描述
         /// </summary>
         [RegularExpression(@"^.{2,20}$", ErrorMessage = "描述必须为2到20位字符!")]
-        public string description { get; set; }
+        public string addDescription { get; set; }
 
         /// <summary>
         /// 排序字段
         /// </summary>
         [RegularExpression(@"^[0-9]*$", ErrorMessage = "排序值必须为数字!")]
-        public int rank { get; set; }
+        public int addRank { get; set; }
     }
 }
